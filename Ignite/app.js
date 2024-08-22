46872782bff6a65a4f726cd0fc4d4f49a5a32935
Debanjanpanigrahi <debanjanp1997@gmail.com>
2024-08-22 12:09:48 +0530

    Ignite our App

---
+++ Ignite/app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading= React.createElement(
+"h2",
+{id:"heading"},
+"H2 in react"
+);
+
+/* heading is an object */
+console.log(heading);
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+/*nested element */
+/*<div id="parent">
+    <div id="child">
+        <h1>h1 tag</h1>
+    </div>
+</div> */
+
+const parent=React.createElement("div",{id:"parent"},
+[
+    React.createElement("div",{id:"child"}),
+[React.createElement("h1",{},"h1 heading"),React.createElement("h1",{},"h2 heading")],
+React.createElement("div",{id:"child"}),
+[React.createElement("h1",{},"h1 heading"),React.createElement("h1",{},"h2 heading")]
+]);
+
+root.render(parent);
